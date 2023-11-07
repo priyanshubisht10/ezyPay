d2eb408518bc3a7460c31e3764ea966b771b1cf2
Priyanshu Bisht <priyanshubisht451@gmail.com>
2023-11-07 14:25:18 +0530

    transfer using Razorpay API

--- app/src/main/java/com/example/ezypay/TransferHandler.java
+++ app/src/main/java/com/example/ezypay/TransferHandler.java
@@ -8,11 +8,10 @@ import com.razorpay.RazorpayException;
 
 public class TransferHandler {
     private static final String TAG = "TransferHandler"; // Log tag
-
     public static void handleTransfer(String paymentId, String amount) {
         // Simulated server-side transfer code
         String keyId = "rzp_test_jSHwMS9oI9FXNi";
-        String keySecret = "UOuQNIbKzmraZXmQ1iWOr1Sq";
+        String keySecret = "UOuQNIbK---------WOr1Sq";
 
         try {
             RazorpayClient razorpay = new RazorpayClient(keyId, keySecret);
@@ -30,8 +29,10 @@ public class TransferHandler {
             razorpay.transfers.create(transferRequest);
             Log.i(TAG, "Transfer initiated for amount: " + amount);
         } catch (RazorpayException | JSONException e) {
+
             Log.e(TAG, "Razorpay Error: " + e.getMessage());
             e.printStackTrace();
+
         }
     }
 }
